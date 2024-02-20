2c73bc43eb6c04b073ce0dd7d77152c633f6ccb9
sondresjolyst <sonyslyst@gmail.com>
2024-02-20 17:52:33 +0100

    AndroidAuto

--- aa_wireless_dongle/package/aawg/src/aawgd.cpp
+++ aa_wireless_dongle/package/aawg/src/aawgd.cpp
@@ -8,7 +8,7 @@
 #include "usb.h"
 
 int main(void) {
-    Logger::instance()->info("AA Wireless Dongle\n");
+    Logger::instance()->info("AndroidAuto\n");
 
     // Global init
     std::optional<std::thread> ueventThread =  UeventMonitor::instance().start();
